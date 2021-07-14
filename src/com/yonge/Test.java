60a33c3b622a9160558216ae2d8dcf01bef5df6d
adaixiwa <1247731621@qq.com>
2021-07-14 15:38:12 +0800

    测试1

--- src/com/yonge/Test.java
+++ src/com/yonge/Test.java
@@ -4,8 +4,13 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("111");
-        
-        
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
         System.out.println("22222");
+
+
+
     }
 }
