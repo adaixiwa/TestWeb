28ae2306527da3c28a23870cee78d2ce196cc5a3
adaixiwa <1247731621@qq.com>
2021-07-14 12:14:51 +0800

    测试

---
+++ src/com/yonge/Test.java
@@ -0,0 +1,11 @@
+package com.yonge;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("111");
+        
+        
+        System.out.println("22222");
+    }
+}
